d348e24f6557e2c62250d28d0d3adb36b0929701
BANELE-MTHEMBU <mphomthembu53@gmail.com>
2023-06-12 20:25:23 +0200

    new changes

--- app.py
+++ app.py
@@ -6,8 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('templates/auth/register',methods=['GET'])
+def register():
+    return render_template('auth/register.html')
+
+@app.route('templates/auth/login',methods=['GET'])
+def login():
+    return render_template('auth/login.html')
 
 if __name__ == "__main__":
     # any port # is acceptable as long it is not in use
     app.run(host = 'localhost', port = 1234, debug = True)
+    
+    
     
---
+++ static/CSS/styles.csc
@@ -0,0 +1,26 @@
+.register,
+.login {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.register-title,
+.login-title {
+  text-align: center;
+}
+.icon-image {
+  width: 30px;
+  margin: 3px auto;
+}
+a {
+  color: white;
+}
+.register-form,
+.login-form {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+}
